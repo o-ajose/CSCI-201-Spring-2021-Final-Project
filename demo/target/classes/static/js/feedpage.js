34ffed1a027dae79f67ca60b0c726d8f17b1dedb
Karen Ly <lykaren27@gmail.com>
2021-04-15 09:55:44 -0700

    Transfer js functions to new branch

---
+++ demo/target/classes/static/js/feedpage.js
@@ -0,0 +1,15 @@
+// When the user clicks on <div>, open the popup and hide posts
+function toggle() {
+    $("#popUp").show();
+    $("#postsSpace").hide();
+}
+
+// remove the create post pop up and show the posts
+function cancel() {
+    $("#popUp").hide();
+    $("#postsSpace").show();
+}
+
+function post() {
+
+}
