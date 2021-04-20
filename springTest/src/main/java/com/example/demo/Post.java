7273b3d0a077397a407d4b14c963f00fc9210541
Oju Ajose <oajose@usc.edu>
2021-04-20 14:23:52 -0700

    Committing Profile edits

---
+++ springTest/src/main/java/com/example/demo/Post.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class Post {
+    public int num;
+    public String URL;
+    public String comments;
+    public String username;
+    Post(int n, String u, String c, String us){
+        num = n;
+        URL = u;
+        comments =c;
+        username = us;
+    }
+    Post(){
+        num = -1;
+        comments = "Please log in again; session expired.";
+    }
+}
