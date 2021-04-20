538b7f0cfff260b1c1dfc1a2be5eb8c4adcec349
Allison Ohara <aohara@usc.edu>
2021-04-19 20:18:23 -0700

    Update UserController.java

--- springTest/src/main/java/com/example/demo/UserController.java
+++ springTest/src/main/java/com/example/demo/UserController.java
@@ -35,13 +35,14 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String register(HttpServletRequest request,@RequestParam String username1, @RequestParam String password1, @RequestParam String email1, @RequestParam String bio, @RequestParam String location, @RequestParam("file") MultipartFile multipartFile) throws InterruptedException, ExecutionException {
-        logger.info("HIT -/upload | File Name : {}"+multipartFile.getOriginalFilename());
-        String profilePic = fileService.upload(multipartFile);
-        User newUser = new User(username1,email1,password1,bio,location,profilePic);
+    public String register(HttpServletRequest request,@RequestParam String username1, @RequestParam String password1, @RequestParam String email1, @RequestParam String bio, @RequestParam String location /*, @RequestParam("file") MultipartFile multipartFile*/) throws InterruptedException, ExecutionException {
+        //logger.info("HIT -/upload | File Name : {}"+multipartFile.getOriginalFilename());
+       // String profilePic = fileService.upload(multipartFile);
+        User newUser = new User(username1,email1,password1,bio,location,"https://firebasestorage.googleapis.com/v0/b/csci201finalproject-310216/o/eb937cd9-37f7-45af-87ed-7533f7f1826b.jpg?alt=media");
         HttpSession session = request.getSession();
         //return (String)session.getAttribute("username");
         session.setAttribute("username",username1);
+        //session.setAttribute("profilePic","https://firebasestorage.googleapis.com/v0/b/csci201finalproject-310216/o/eb937cd9-37f7-45af-87ed-7533f7f1826b.jpg?alt=media")
         return userService.saveUserDetails(newUser);
     }
 
