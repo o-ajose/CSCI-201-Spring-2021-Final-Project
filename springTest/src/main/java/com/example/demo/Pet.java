0003d6db9fa414d420d2ff44a971bd9c6c5f0409
Allison Ohara <aohara@usc.edu>
2021-04-22 14:54:10 -0700

    Update Pet.java

--- springTest/src/main/java/com/example/demo/Pet.java
+++ springTest/src/main/java/com/example/demo/Pet.java
@@ -8,6 +8,8 @@ public /*abstract*/ class Pet {
     public int age;
     private String breed;
     private String relationshipStatus;
+    private String bio;
+    private String pic;
     private String size;
 
     public Pet(){
@@ -17,16 +19,30 @@ public /*abstract*/ class Pet {
         breed = "BROKEN";
         relationshipStatus = "BROKEN";
         size = "BROKEN";
+        bio="BROKEN";
+        pic ="BROKEN";
     }
 
     public Pet(String name,int age){
         this.name = name;
         this.age = age;
     }
+    public Pet(String name, int age, String personality, String breed, String rS, String size, String bio,String pic){
+        this.name = name;
+        this.age = age;
+        this.personality = personality;
+        this.breed = breed;
+        this.relationshipStatus =rS;
+        this.size = size;
+        this.bio = bio;
+        this.pic = pic;
+    }
 
     void setName(String name){
         this.name = name;
     }
+    void setBio(String b){bio = b;}
+    void setPic(String p){pic = p;}
     void setPersonality(String personality){
         this.personality = personality;
     }
@@ -42,6 +58,8 @@ public /*abstract*/ class Pet {
     void setSize(String size){
         this.size = size;
     }
+    public String getPic(){return pic;}
+    public String getBio(){return bio;}
     public String getName(){
         return name;
     }
