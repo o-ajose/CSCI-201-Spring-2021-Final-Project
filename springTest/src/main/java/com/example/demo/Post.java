5ba456719854b2ca53439d26751e35ee849aa249
Karen Ly <lykaren27@gmail.com>
2021-04-23 09:46:27 -0700

    Update Post.java

--- springTest/src/main/java/com/example/demo/Post.java
+++ springTest/src/main/java/com/example/demo/Post.java
@@ -13,6 +13,6 @@ public class Post {
     }
     Post(){
         num = -1;
-    comments = "Please log in again; session expired.";
+        comments = "Please log in again; session expired.";
     }
 }
