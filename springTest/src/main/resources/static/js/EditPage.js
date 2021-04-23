4d6194065a6ee46cde9dcf461addc80a39c25db5
Oju Ajose <oajose@usc.edu>
2021-04-22 19:49:15 -0700

    Profile Pages w/ servlet connections + Karen's new navBar

--- springTest/src/main/resources/static/js/EditPage.js
+++ springTest/src/main/resources/static/js/EditPage.js
@@ -137,3 +137,31 @@ function pullUserInfo() {
         }
     });
 }
+
+/* *** Functions for nav bar redirections *** */
+// logs user out
+function logout() {
+    console.log("Logging user out");
+}
+
+// redirects to account page
+function getAccountPage() {
+    console.log("Redirecting to account page");
+    location.href = "ProfilePage.html";
+}
+
+// nav bar redirects to different pages
+function goToFriends() {
+    console.log("Redirecting to friends page");
+    location.href = "friendsPage.html";
+}
+// nav bar redirects to different pages
+function goToExplore() {
+    console.log("Redirecting to explore page");
+    location.href = "explorepage.html";
+}
+// nav bar redirects to different pages
+function goToFeed() {
+    console.log("Redirecting to explore page");
+    location.href = "feedpage.html";
+}
--- springTest/src/main/resources/static/js/EditPets.js
+++ springTest/src/main/resources/static/js/EditPets.js
@@ -107,3 +107,31 @@ function createNewElement() {
     document.getElementById("updatePets").style.display = "none";
 
 }
+
+/* *** Functions for redirecting from navBar **/
+// logs user out
+function logout() {
+    console.log("Logging user out");
+}
+
+// redirects to account page
+function getAccountPage() {
+    console.log("Redirecting to account page");
+    location.href = "ProfilePage.html";
+}
+
+// nav bar redirects to different pages
+function goToFriends() {
+    console.log("Redirecting to friends page");
+    location.href = "friendsPage.html";
+}
+// nav bar redirects to different pages
+function goToExplore() {
+    console.log("Redirecting to explore page");
+    location.href = "explorepage.html";
+}
+// nav bar redirects to different pages
+function goToFeed() {
+    console.log("Redirecting to explore page");
+    location.href = "feedpage.html";
+}
