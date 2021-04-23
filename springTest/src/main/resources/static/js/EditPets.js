6bc7e73d5015ab482e92f649c79aeebc1e142b29
Oju Ajose <oajose@usc.edu>
2021-04-22 19:35:57 -0700

    Profile Pages + assoc with servlet connections

---
+++ springTest/src/main/resources/static/js/EditPets.js
@@ -0,0 +1,109 @@
+/* TODO:  - Start adding Karen's code for the nav pages?
+ */
+
+var numPets = 0;
+// Function to get all pet details and place them accordingly on the page
+function getPetDetails() {
+    console.log("Calling pullPet function for updates!!");
+
+    $.ajax({
+        type: 'POST',
+        url: 'getAllPetDetails',
+        // dataType: ''
+        success: function(response) {
+            console.log("retrieved pet objects");
+            console.log(response);
+            const petsArray = response;
+            numPets = petsArray.length;
+            var petDiv = document.getElementById("row");
+            for(let i = 0; i < petsArray.length; i++) {
+                petDiv.innerHTML +=
+                    "<div class=\"column\" id=\'pet" + (i+1) + "\'>\n" +
+                    "    <p>Change " + petsArray[i].name + "\'s Profile Photo: </p>\n" +
+                    "    <div style=\"display: flex\">\n" +
+                    "        <label for=\"pet" + (i+1) + "Avi\"><img class=\"userImage\" src=\"" + petsArray[i].URL + "\"></label>\n" +
+                    "        <input style=\"display: none;\" type=\"file\" id=\"pet" + (i+1) + "Avi\" name=\"pet" + (i+1) + "Avi\" accept=\"image/*\">\n" +
+                    "    </div>\n" +
+                    "    <br/>\n" +
+                    "    Name <input type=\"text\" name=\"name\" placeholder=\"" + petsArray[i].name + "\" REQUIRED/> <br>\n" +
+                    "    Age <input type=\"text\" name=\"age\" placeholder=\"" + petsArray[i].age + " years\"/> <br>\n" +
+                    "    <label for=\"size\">Size</label>\n" +
+                    "    <select name=\"size\" id=\"size\">\n" +
+                    "        <option value=\"small\">Small </option>\n" +
+                    "        <option value=\"medium\">Medium</option>\n" +
+                    "        <option value=\"large\">Large </option>\n" +
+                    "     </select><br>\n" +
+                    "     Breed <input type=\'text\' name=\'breed\' placeholder=\"" + petsArray[i].breed + "\" required/> <br>\n" +
+                    "    <p class=\"formfield\">\n" +
+                    "        <label for=\"personality" + (i+1) + "\" class=\"alignTop\"> Personality</label>\n" +
+                    "        <textarea id=\"personality" + (i+1) + "\" name=\"personality" + (i+1) + "\" rows=\"3\" cols=\"50\" placeholder=\"" + petsArray[i].personality + "\"></textarea>\n" +
+                    "    </p>\n" +
+                    "    <p class=\"formfield\">\n" +
+                    "        <label for=\"relStatus" + (i+1) + "\" style=\"height: 50px;\"> Relationship Status</label>\n" +
+                    "        <textarea id=\"relStatus" + (i+1) + "\" name=\"relStatus" + (i+1) + "\" rows=\"3\" cols=\"50\" placeholder=\"" + petsArray[i].relationshipStatus + "\"></textarea>\n" +
+                    "    </p>\n" +
+                    "</div>\n" +
+                    "<br>";
+            }
+        },
+        error: function() {
+            console.log("could not retrieve pet data");
+        }
+    });
+}
+
+// Function to update the pets
+function updatePets() {
+    console.log("Calling updatePets function!");
+    for(let i =0; i< numPets; i++) {
+        // gets the data within that id and creates an object that updatePets can understand
+        var nameID = 'name' + (i+1);
+        var ageID = 'age' + (i+1);
+        var breedID = 'breed' + (i+1);
+        var sizeID = 'size' + (i+1);
+        var relationshipStatusID = 'relationshipStatus' + (i+1);
+        var personalityID = 'personality' + (i+1);
+        var petPic = 'pet' + (i+1) + 'Avi';
+        var pet = {
+            name: document.getElementById(nameID).valueOf(),
+            age: document.getElementById(ageID).valueOf(),
+            breed: document.getElementById(breedID).valueOf(),
+            size: document.getElementById(sizeID).valueOf(),
+            relationshipStatus: document.getElementById(relationshipStatusID).valueOf(),
+            personality: document.getElementById(personalityID).valueOf(),
+            profilePic: document.getElementById(petPic).valueOf()
+        };
+        updatePet(pet); // calls update Pet on that object to send over pets one at a time
+
+        console.log(pet);
+
+    }
+
+}
+
+function updatePet(dataObj) {
+    $.ajax({
+        type: 'POST',
+        url: 'updatePet',
+        contentType: 'application/json',
+        data: JSON.stringify(dataObj),
+        success: function() {
+            var msg = 'Saved ' + dataObj.name + '\'s info!'
+            console.log(msg);
+        },
+        error: function(response) {
+            console.log(response);
+        }
+    });
+}
+
+/* If the user tries to add a new pet, they can only update the pet and not also
+        their other pets to save us a headache and time */
+function createNewElement() {
+    const newInputBox = document.getElementById("newPetInfo").innerHTML.toString();
+    document.getElementById("Pet").innerHTML += newInputBox;
+
+    document.getElementById("addPet").style.display = "none";
+    document.getElementById("updatePets").style.display = "none";
+
+}
---
+++ springTest/src/main/resources/static/js/ProfilePage.js
@@ -0,0 +1,144 @@
+let name; // Creates a global variable that can be accessed by the petInfo function when printing out the owner's name
+
+// Function to call user info to be printed out
+function pullUserInfo() {
+    console.log("Calling fetchUser function!");
+    var userInfo;
+
+    $.ajax({
+        type: 'POST',
+        url: 'fetchUserProfile',
+        // dataType: ''
+        success: function(response) {
+            console.log("retrieved user object");
+            console.log(response);
+
+            const userInfo = response;
+            const username = userInfo.username;
+            name = userInfo.name;
+            const profilePic = userInfo.profilePic;
+            let bio = userInfo.bio;
+            const location = userInfo.location;
+            const song = userInfo.song;
+
+            var userDiv = document.getElementById("UserContainer");
+            // Checks if the return value is not null before printing it out
+            document.getElementById("UserContainer").innerHTML =
+                "<div class=\"userImage\">\n"+
+                "    <img src=\"" + profilePic + "\" alt=\"" + name + "'s Profile Picture\">\n" +
+                "</div>\n" +
+                "<div class=\"userText\">\n";
+            if(username !== "") {
+                userDiv.innerHTML +=  "    <p>" + username + "</p>\n";
+            }
+            userDiv.innerHTML +=
+                "    <p>Name: " + name + "</p>\n";
+            if(bio !== "") {
+                userDiv.innerHTML +=  "    <p>Biography: " + bio + "</p>";
+            }
+            userDiv.innerHTML +=
+                "    <p>Location: " + location + "</p>\n" +
+                "</div>\n" +
+                "<br>";
+        },
+        error: function() {
+            console.log("could not retrieve user data");
+        }
+    });
+
+}
+
+// Function to get pet info
+function pullPetInfo() {
+    console.log("Calling pullPet function!");
+    var petsArray;
+
+    $.ajax({
+        type: 'POST',
+        url: 'getAllPetDetails',
+        // dataType: ''
+        success: function(response) {
+            console.log("retrived pet objects");
+            console.log(response);
+            const petsArray = response;
+
+            document.getElementById("petHeader").innerHTML = name + "\'s Pets";
+
+            var petDiv = document.getElementById("row");
+            for(let i = 0; i < petsArray.length; i++) {
+                // iterate through the array, and after every two pets, add a breakline for a new row
+                if(i > 0 && i % 2 === 1) {
+                    // we need to add a breakline
+                    petDiv.innerHTML += "<br>\n";
+                }
+                petDiv.innerHTML +=
+                    "<div class=\"column\">" +
+                    "    <div class=\"image\">\n" +
+                    "        <img src=\"" + petsArray[i].URL + "\" alt=\"" + petsArray[i].name + "\">\n" +
+                    "    </div>\n" +
+                    "    <div class=\"text\">\n" +
+                    "        <p>Name: " + petsArray[i].name + "</p>\n" +
+                    "        <p>Age: " + petsArray[i].age + " years </p>\n" +
+                    "        <p>Breed: " + petsArray[i].breed + "</p>\n" +
+                    "        <p>Size: " + petsArray[i].size + "</p>\n" +
+                    "        <p>Personality: " + petsArray[i].personality + "</p>\n" +
+                    "        <p>Relationship status: " + petsArray.relationshipStatus + "</p>\n" +
+                    "    </div>\n" +
+                    "</div>";
+            }
+        },
+        error: function() {
+            console.log("could not retrieve pet data");
+        }
+    });
+}
+
+// Get the song info and set up the javascript play function
+function pullSongInfo() {
+    console.log("calling getSong!");
+
+    // Map to connect the filenames from the servlet to the actual names of each song
+    let songMap = new Map();
+    songMap.set('Music/All%20You%20Need%20Is%Love.mp3', 'All You Need Is Love - The Beatles');
+    songMap.set('Music/Dog.mp3', 'Dog - Remo Drive');
+    songMap.set('Music/Dog%20%Days%20Are%20Over.mp3', 'Dog Days Are Over - Florence + The Machine');
+    songMap.set('Music/Dynamite.mp3', 'Dynamite - BTS');
+    songMap.set('Music/Loving%20Is%20Easy.mp3', 'Loving Is Easy - Rex Orange County');
+    songMap.set('Music/Me%20&%20You%20Together%20Song.mp3', "Me & You Together Song - The 1975");
+    songMap.set('Music/Mooo!.mp3', 'Mooo! - Doja Cat');
+    songMap.set('Music/Watermelon%20Sugar.mp3', 'Watermelon Sugar - Harry Styles');
+    songMap.set('Music/What\'s%20New%20Pussycat.mp3', 'What\'s New Pussycat - Tom Jones');
+
+    $.ajax({
+        type: 'POST',
+        url: 'getSong',
+        // dataType: string representing the fileName
+        success: function(response) {
+            console.log("retrieved song object");
+            console.log(response);
+            const song = response.song;
+            const songName = songMap.get(song);
+
+            document.getElementById("audio").innerHTML =
+                "<audio id=\"song\" src=\"" + song + "\" preload=\"auto\">\n" +
+                "<p>If you are reading this, it is because your browser does not support the audio element.</p>\n" +
+                "</audio>\n" +
+                "<button class=\"button\" onclick=\"playPause()\">Chosen Song: " + songName + "</button>";
+        },
+        error: function() {
+            console.log("could not retrieve song data");
+        }
+    });
+}
+
+/* Script to play the song */
+function playPause() {
+    var play = document.getElementById("song");
+    return play.paused ? play.play() : play.pause();
+}
+
+/* TODO: button that checks if the user visiting the page is the owner of the page to see if the "AddFriend" button
+*       or the "EditProfile" button should be displayed */
+function setButton() {
+
+}
