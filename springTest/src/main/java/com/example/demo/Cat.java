c0145f8d23c60036863d944e26648e133ee0580e
Karen Ly <lykaren27@gmail.com>
2021-04-23 17:38:05 -0700

    Remove old files

--- springTest/src/main/java/com/example/demo/Cat.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-public class Cat extends Pet{
-    Cat(String name, int age){
-        super(name,age);
-    }
-    public int getAge(){
-        return age*15;
-    }
-}
--- springTest/src/main/java/com/example/demo/Dog.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-public class Dog extends Pet{
-    Dog(String name, int age){
-        super(name,age);
-    }
-    public int getAge(){
-        return age*7;
-    }
-}
