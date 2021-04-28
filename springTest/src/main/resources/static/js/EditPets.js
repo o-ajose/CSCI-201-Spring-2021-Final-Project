186c43f9c365358af251be90f059bd8cade7f882
Allison Ohara <aohara@usc.edu>
2021-04-27 20:03:36 -0700

    Update EditPets.js

--- springTest/src/main/resources/static/js/EditPets.js
+++ springTest/src/main/resources/static/js/EditPets.js
@@ -3,6 +3,7 @@ var name;
 var age;
 var breed;
 var personality;
+var petsArray;
 var relationshipStatus;
 var bio;
 var size;
@@ -16,7 +17,7 @@ function getPetDetails() {
         success: function(response) {
             console.log("retrieved pet objects");
             console.log(response);
-            const petsArray = response.petList;
+            petsArray = response.petList;
             if(petsArray.length === 0) {
                 document.getElementById("row").innerHTML += "No pets added yet!";
                 return false;
@@ -30,11 +31,28 @@ function getPetDetails() {
             relationshipStatus = petsArray[0].relationshipStatus;
             bio = petsArray[0].bio;
             var petDiv = document.getElementById("row");
+            /*
+            var oldSel = $('.choosePet').get(0);
+            for (i = 0; i < petsArray.length; i++)
+            {
+                var opt = document.createElement('option');
+
+                opt.text = petsArray[i].name;
+                opt.value = i;
+
+                oldSel.add(opt, null);
+            }
+            */
+            for (i = 0; i < petsArray.length; i++) {
+                $('#choosePet').append($("<option></option>").attr("value", i).text(petsArray[i].name));
+            }
+            $('#choosePet').val(0);
+            /*
             if(petsArray.length === 1) {
                 document.getElementById("addPet").style.display = "none";
-            }
-            for(let i = 0; i < petsArray.length; i++) {
-                petDiv.innerHTML +=
+            }*/
+            for(let i = 0; i < 1; i++) {
+                petDiv.innerHTML =
                     "<div class=\"column\" id=\'pet" + (i+1) + "\'>\n" +
                     "    <p>Change " + petsArray[i].name + "\'s Profile Photo: </p>\n" +
                     "    <div style=\"display: flex\">\n" +
@@ -72,12 +90,47 @@ function getPetDetails() {
     });
     return false;
 }
+function updateForm(){
+    var petDiv = document.getElementById("row");
+    var i = $('#choosePet').val();
+    petDiv.innerHTML =
+        "<div class=\"column\" id=\'pet" + (i+1) + "\'>\n" +
+        "    <p>Change " + petsArray[i].name + "\'s Profile Photo: </p>\n" +
+        "    <div style=\"display: flex\">\n" +
+        "        <label for=\"pic\"><img class=\"userImage\" src=\"" + petsArray[i].pic + "\"> No more than 1 MB</label>\n" +
+        "        <input type=\"file\" id=\"pic\" name=\"pic\" accept=\"image/*\">\n" +
+        "    </div>\n" +
+        "    <br/>\n" +
+        "    Name <input type=\"text\" name=\"name\" value=\'" + petsArray[i].name + "\' placeholder=\"" + petsArray[i].name + "\" required/> <br>\n" +
+        "    Age (only enter the number) <input type=\"number\" name=\"age\" placeholder=\"" + petsArray[i].age + " years\"/> <br>\n" +
+        "    <label for=\"size\">Size</label>\n" +
+        "    <select name=\"size\" id=\"size\">\n" +
+        "        <option value=\"small\">Small </option>\n" +
+        "        <option value=\"medium\">Medium</option>\n" +
+        "        <option value=\"large\">Large </option>\n" +
+        "     </select><br>\n" +
+        "     Breed <input type=\'text\' name=\'breed\' placeholder=\"" + petsArray[i].breed + "\"/> <br>\n" +
+        "    <p class=\"formfield\">\n" +
+        "        <label for=\"personality\" class=\"alignTop\"> Personality</label>\n" +
+        "        <textarea id=\"personality\" name=\"personality\" rows=\"3\" cols=\"50\" placeholder=\"" + petsArray[i].personality + "\"></textarea>\n" +
+        "    </p>\n" +
+        "    <p class=\"formfield\">\n" +
+        "        <label for=\"relationshipStatus\" style=\"height: 50px;\"> Relationship Status</label>\n" +
+        "        <textarea id=\"relationshipStatus\" name=\"relationshipStatus\" rows=\"3\" cols=\"50\" placeholder=\"" + petsArray[i].relationshipStatus + "\"></textarea>\n" +
+        "    </p>\n" +
+        "    <p class=\"formfield\">\n" +
+        "        <label for=\"bio1\" style=\"height: 50px;\"> Bio</label>\n" +
+        "        <textarea id=\"bio1\" name=\"bio1\" rows=\"3\" cols=\"50\" placeholder=\"" + petsArray[i].bio + "\"></textarea>\n" +
+        "    </p>\n" +
+        "</div>";
+}
 
 // Function to update the pets
 function updatePets() {
     console.log("Calling updatePets function!");
     var form = $('#updatePets')[0];
     var data = new FormData(form);
+
     console.log(data);
     // add in formData function and update the ID of bio1 in the form
     $.ajax({
@@ -93,7 +146,7 @@ function updatePets() {
             console.log(response);
             console.log('Saved pet details!');
             alert('Saved pet details!');
-            window.location.href = '../ProfilePage.html';
+            window.location.href = "ProfilePage.html";
         },
         error: function() {
             console.log("Error found, could not save pet");
@@ -137,6 +190,7 @@ function updatePet(dataObj) {
         success: function() {
             var msg = 'Saved ' + dataObj.name + '\'s info!'
             console.log(msg);
+
         },
         error: function(response) {
             console.log(response);
@@ -162,7 +216,7 @@ function createPet() {
         success: function(response) {
             console.log(response);
             console.log('Saved new pet!');
-            window.location.href = '../ProfilePage.html';
+            window.location.href = "ProfilePage.html";
         },
         error: function() {
             console.log("Error found, could not save pet");
@@ -173,17 +227,22 @@ function createPet() {
 /* If the user tries to add a new pet, they can only update the pet and not also
         their other pets to save us a headache and time */
 function createNewElement() {
-    const newInputBox = document.getElementById("newPetInfo").innerHTML.toString();
+    $('#newPetInfo').show();
+  //  const newInputBox = document.getElementById("newPetInfo").innerHTML.toString();
+    /*
     document.getElementById("Pet").innerHTML += newInputBox;
 
     document.getElementById("addPet").style.display = "none";
     document.getElementById("updatePets").style.display = "none";
     document.getElementById("Edit").style.display="block";
 
+     */
+
 }
 
 /* *** Functions for redirecting from navBar **/
 function getFeed() {
+
     // load user's name and image and posts
     $.ajax({
         type: 'POST',
@@ -196,6 +255,7 @@ function getFeed() {
             document.getElementById("userBtn").innerHTML = response.username;
         }
     });
+    $('#newPetInfo').hide();
 }
 
 // logs user out
